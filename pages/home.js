90c180ca5007cd855c7811fac4f55dbc3cac3b25
kmatlaha <katematltest@gmail.com>
2022-12-28 22:01:49 +0200

    Fixed final scenario

--- pages/home.js
+++ pages/home.js
@@ -1,11 +1,11 @@
 const { I } = inject();
-const Helper = require('../helpers/helper.js'); 
 
 module.exports = {
   myAccountSpoiler: { xpath: '//*[@id="top-links"]/ul/li/span/span' },
   registerLink: { xpath: '//a[.="Register"]' },
   emptyCartText: { xpath: "//p[text() = 'Your shopping cart is empty!']" },
   cartIcon: { css: "i.linearicons-cart" },
+  trashBin: {css: 'i.linearicons-trash'},
 
   openRegistrationPage() {
     I.click(this.myAccountSpoiler);
@@ -14,10 +14,9 @@ module.exports = {
 
   async emptyCart() {
     I.click(this.cartIcon);
-    let numOfElements = await I.grabNumberOfVisibleElements({css: 'i.linearicons-trash'}, 'class');
+    let numOfElements = await I.grabNumberOfVisibleElements(this.trashBin);
       for(let i=numOfElements; i>0; i--) {
             I.click({xpath: `(//i[@class="linearicons-trash"])[${i}]`});
         };
-    //}
   },
 }
